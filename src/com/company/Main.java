63db79aa02c2f347cd26ad6af8f7e7b5a76be082
lubu21 <lukas.buchacher@edu.campus02.at>
2019-11-14 20:41:50 +0100

    neue Code-Zeile

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,10 @@ public class Main {
 	// write your code here
       System.out.println("Hello World");
       System.out.println("Hier steht eine zweite Zeile");
+
+      int a=38;
+      int b=55;
+      System.out.println("a = " + a + " b = " + b);
+      System.out.println("a + b = " + (a+b));
     }
 }
